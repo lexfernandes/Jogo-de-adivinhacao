dd561b128b99172ede4858dea4bee7e9b048d8b3
“lexfernandes” <lexlopesfernandes@gmail.com>
2023-07-31 21:18:42 -0300

    Estudo JavaScript

---
+++ functions/functions.js
@@ -0,0 +1,24 @@
+// operadores de atribuição (assignment)
+let x
+
+// assignment
+x = 1
+
+// addition assignment
+// x = x + 2
+x +=2
+
+//subtraction assignment
+x-=1
+console.log(x)
+
+//multiplication assignment
+x *=2
+
+// division assignment
+ x /= 2
+
+ //remainder, exponetiation
+ x %= 2
+
+ x **=2
