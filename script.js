ea30ea55b7cf43a2767280235772aad375d04af4
“lexfernandes” <lexlopesfernandes@gmail.com>
2023-08-30 21:20:11 -0300

    comentarios

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
 const btnTry = document.querySelector("#btnTry")
 const btnReset = document.querySelector("#btnReset")
+// criação de um número randômico.
 const randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
 // Eventos
+// aguardando usuário clicar em tentar para chamar a função handleTryClick.
 btnTry.addEventListener("click", handleTryClick)
 btnReset.addEventListener("click", handleReset)
 document.addEventListener("keypress", function (e) {
@@ -22,10 +24,13 @@ function handleTryClick(event) {
   const inputNumber = document.querySelector("#inputNumber")
   // vai comparar se o input é igual o número random na constante lado de fora.
   if (Number(inputNumber.value) == randomNumber) {
+    // input.value serve para pegar o valor do input.
     screen1.classList.add("hide") // adiciona o hide na div screen1
     screen2.classList.remove("hide") // remove o hide na div screen2
     // adiciona o texto na segunda tela com número de tentativas
-    screen2.querySelector("h2").innerText = `Você adivinhou o número em ${xAttempts} tentativas`
+    screen2.querySelector(
+      "h2"
+    ).innerText = `Você adivinhou o número em ${xAttempts} tentativas`
   }
   // esvazia o imput para vazio
   inputNumber.value = ""
